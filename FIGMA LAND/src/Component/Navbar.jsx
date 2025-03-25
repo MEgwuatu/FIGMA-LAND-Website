4143c2d436f8067456e805ea01b504b4a10bdadf
egwuatu modestus <egwuatumodestus@gmail.com>
2025-03-25 09:38:50 +0100

    Updated 25/03/2025: 9:38am

--- FIGMA LAND/src/Component/Navbar.jsx
+++ FIGMA LAND/src/Component/Navbar.jsx
@@ -27,8 +27,8 @@ const Navbar = () => {
             </div>
         ))}
         </div>
-        <button className='w-[139px] h-[50px] rounded-[3px] border-[1px] mt-2'>Login</button>
-        </nav>\
+        <button className='w-[139px] h-[45px] rounded-[3px] border-[1px] mt-2'>Login</button>
+        </nav>
 
         {/* mobile responsiveness */}
         <nav className='z-10 md:hidden flex justify-center w-[90vw] fixed'>
@@ -38,7 +38,7 @@ const Navbar = () => {
                 <img src={logo} alt="logo" className='w[100%] pt-2' />                
             </div>
 
-            <button className='w-[130px] h-[45px] rounded-[3px] border-[1px] mt-2'>Login</button>
+            <button className='w-[130px] h-[45px] text-[white] rounded-[3px] border-[1px] mt-2'>Login</button>
 
             <div className=" w-[10%] pt-3  pl-4" onClick={handleMenuClick}>
          {isMenuOpen ? (
@@ -52,6 +52,44 @@ const Navbar = () => {
         </div>
 
       </nav>
+
+      {/* Dropdown functionality */}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.aside
+          initial={{width: 0}}
+            animate={{
+              width: 500
+            }}
+            exit={{
+              width: 0,
+              transmission: {delay: 0.7, duration: 0.3}
+            }}
+            >
+              <motion.div 
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={sideVariants}
+              className="nav-container md:hidden border flex flex-col fixed text-white w-[55vw]  mt-[120px] h-[350px] justify-around 
+              items-end  pr-[18vw] ml-[28%] z-10 rounded-2xl pt-4"
+              >
+                 <div className="flex flex-col   items-center w-[30%]">
+                 {navlinks.map((link) => (
+                   <motion.div className="flex flex-col pr-7 text-[16px]" key={link.name}>
+                     <motion.a href="#" variants={itemVariants} className='list-none p-3'>{link.title}</motion.a>
+                   </motion.div>
+                 ))}
+                  </div>
+                      <motion.button
+                    variants={itemVariants}
+                    className="w-[150px] h-[48px] text-black bg-white font-bold rounded-xl mb-7 text-[14px] mr-[-2.8em]">
+                    Get Started
+                 </motion.button>
+            </motion.div>           
+          </motion.aside>
+              )}       
+      </AnimatePresence>
 
 
     </>
