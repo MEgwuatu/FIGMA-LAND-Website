cd43f7db78674c98cc1a59961789d01e4be74d0b
egwuatu modestus <egwuatumodestus@gmail.com>
2025-03-28 10:31:02 +0100

    Updated 28/03/2025 10:30am

---
+++ FIGMA LAND/src/Pages/Home.jsx
@@ -0,0 +1,7 @@
+import Hero from "./Hero";
+import Features from "../Component/Feature";
+import Contents from "../Component/Contents";
+import Gallery from "../Component/Gallery";
+import Partners from "../Component/Partners";
+import Testimonials from "../Component/Testimonials";
+import Try from "../Component/Try";
